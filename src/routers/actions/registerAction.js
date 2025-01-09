0b7cd28351ad0fabfc8c52eee1172c89a436dd87
Akshat Saini <axshat.ind@gmail.com>
2025-01-09 13:25:48 +0530

    change

--- src/routers/actions/registerAction.js
+++ src/routers/actions/registerAction.js
@@ -1,13 +1,25 @@
 /* Custom Modules */
 import { account } from '../../lib/appwrite';
+import generateID from '../../utils/generateID';
 
 /* Handles user registration */
 const registerAction = async ({ request }) => {
   /* retrieve the form data from the incoming request */
   const formData = await request.formData();
-  console.log(formData.get('name'));
-  console.log(formData.get('email'));
-  console.log(formData.get('password'));
+  try {
+    //Creates a new user account using the provided email, password and name
+    await account.create(
+      generateID(), //Generates a unique ID for the user
+      formData.get('email'), //Retrieves email from form data
+      formData.get('password'), //Retrieves password from form data
+      formData.get('name'), //Retrieves name from form data
+    );
+  } catch (err) {
+    //Return an error object if account creation fails
+    return {
+      message: err.message, //Error message from the caught error
+    };
+  }
   return null;
 };
 
