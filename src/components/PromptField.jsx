621edf24dc7e80e0cb0c6973a4f1b086f9c7294a
Akshat Saini <axshat.ind@gmail.com>
2025-03-02 19:40:51 +0530

    fixed

--- src/components/PromptField.jsx
+++ src/components/PromptField.jsx
@@ -85,7 +85,7 @@ const PromptField = () => {
         when: 'beforeChildren',
         staggerChildren: 0.2,
         duration: 0.4,
-        delaay: 0.4,
+        delay: 0.4,
         ease: [0.05, 0.7, 0.1, 1],
       },
     },
@@ -110,8 +110,8 @@ const PromptField = () => {
         contentEditable={true}
         role='textbox'
         aria-multiline={true}
-        aria-label="What's on your mind?"
-        data-placeholder="What's on your mind?"
+        aria-label='Let me'
+        data-placeholder='Let me do the lifting!'
         variants={promptFieldChildrenVariant}
         ref={inputField}
         onInput={handleInputChange}
